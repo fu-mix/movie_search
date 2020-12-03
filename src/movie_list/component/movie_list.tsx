db6c2fff01d99da470ed078e360fa457762cd73e
fumi <fuminaka@gmail.com>
2020-12-03 18:37:56 +0900

    add simple movie search function

--- src/movie_list/component/movie_list.tsx
+++ src/movie_list/component/movie_list.tsx
@@ -1,9 +1,36 @@
-import * as React from "react"
-import {MovieItem} from "../../movie_item/component/movie_item"
+import * as React from 'react';
+// import { MovieItem } from '../../movie_item/component/movie_item';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+import { Movie } from '../../App';
 
-const MovieList:React.FC<{}>=({})=>{
+const DEFAULT_PLACEHOLDER_IMAGE =
+  'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
 
-    return (
-        
-    )
+interface MovieProps {
+  movie: Movie;
 }
+const MovieList: React.FC<MovieProps> = ({ movie }) => {
+  const poster =
+    movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+  return (
+    <Card
+      style={{
+        width: 200,
+        marginTop: '8px',
+      }}>
+      <CardMedia
+        style={{
+          height: 300,
+        }}
+        image={poster}
+        title={`The movie titled: ${movie.Title}`}
+        component="img"
+      />
+      <CardContent>
+        <Typography component="h2">{movie.Title}</Typography>
+        <Typography component="p">{movie.Year}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+export default MovieList;
